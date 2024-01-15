029379accdaead033d09cfd1f30da802a6e94b53
ButWhyFi <97642173+ButWhyFi@users.noreply.github.com>
2024-01-15 00:55:21 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+
+/**
+ * @author: Johnny Zheng
+ * @Version: May 2022 - June 2022
+ * 
+ * Main class for the Tic Tac Toe program. Sets up the window.
+ */
+
+public class Main extends Application {
+  
+  private int boxSize = 200;
+
+  @Override // Override the start method in the Application class
+  public void start(Stage stage) {
+    stage.setTitle("TicTacToe");
+    stage.setHeight(760);
+    stage.setWidth(630);
+    
+    
+    GridSpace gridLayout = new GridSpace(boxSize);
+    BorderPane bPane = new BorderPane(gridLayout);
+    Scene scene = new Scene(bPane);
+    
+
+    HBox hBox = new HBox();
+    hBox.getChildren().add(gridLayout.newGameMenuBtn);
+    bPane.setBottom(hBox);
+    hBox.setSpacing(10);
+    hBox.setPadding(new Insets(10));
+    hBox.setAlignment(Pos.BOTTOM_CENTER);
+    
+    bPane.setBottom(hBox);
+    bPane.setTop(gridLayout.header);
+    BorderPane.setAlignment(gridLayout.header, Pos.TOP_CENTER);
+    BorderPane.setMargin(gridLayout.header, new Insets(20, 0, 0, 0));
+    
+    gridLayout.header.setText("");
+    gridLayout.header.setFont(Font.font(20));
+    
+    stage.setResizable(false);
+    stage.setAlwaysOnTop(true);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
